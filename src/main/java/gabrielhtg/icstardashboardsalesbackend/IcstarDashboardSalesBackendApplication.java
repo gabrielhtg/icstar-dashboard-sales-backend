b1afb2cee2b929eb9d15a4ad5c4163fb0d7df2a8
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2023-09-12 21:21:26 +0700

    initial commit

---
+++ src/main/java/gabrielhtg/icstardashboardsalesbackend/IcstarDashboardSalesBackendApplication.java
@@ -0,0 +1,13 @@
+package gabrielhtg.icstardashboardsalesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcstarDashboardSalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcstarDashboardSalesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabrielhtg/icstardashboardsalesbackend/IcstarDashboardSalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielhtg.icstardashboardsalesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcstarDashboardSalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
